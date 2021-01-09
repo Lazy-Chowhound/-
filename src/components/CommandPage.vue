c82c061f96b614ee1b80c59b8c226b46dd89c810
Lazy-Chowhound <1103036472@qq.com>
2021-01-09 17:25:09 +0800

    将卡片初步组件化

--- src/components/CommandPage.vue
+++ src/components/CommandPage.vue
@@ -1,23 +1,66 @@
 <template>
   <div>
-    <el-collapse-transition>
-      <div v-show="show">
-        <div class="main">
-          <el-input type="textarea" ref="input" class="commandBoard" rows="20" v-model="command" resize="none"
-                    v-on:keydown.delete.stop="banDelete($event)" v-loading="loading" ></el-input>
-          <el-button type="primary" v-on:click="sendCommand" icon="el-icon-thumb" :loading="loading">开始配置</el-button>
-          <el-button type="primary" v-on:click="clearAll" icon="el-icon-remove-outline">清除所有</el-button>
-        </div>
-      </div>
-    </el-collapse-transition>
+    <div v-show="show">
+      <el-container class="container">
+        <el-aside class="asideArea" width="600px">
+          <div class="configurationArea">
 
+            <el-tabs type="border-card" :stretch="true">
+              <el-tab-pane>
+                <span slot="label"><i class="el-icon-s-tools"></i>路由器A</span>
+                <ipcard name="路由器A"></ipcard>
+              </el-tab-pane>
+              <el-tab-pane>
+                <span slot="label"><i class="el-icon-s-tools"></i>路由器B</span>
+                <ipcard name="路由器B"></ipcard>
+              </el-tab-pane>
+              <el-tab-pane label="验证">
+                <span slot="label"><i class="el-icon-info"></i>验证</span>
+                <el-card class="box-card">
+                  <div slot="header">验证</div>
+                  <div class="item">验证</div>
+                </el-card>
+              </el-tab-pane>
+              <el-tab-pane label="待定">
+                <el-card class="box-card">
+                  <div slot="header">待定</div>
+                  <div class="item">待定</div>
+                </el-card>
+              </el-tab-pane>
+            </el-tabs>
+
+          </div>
+        </el-aside>
+        <el-main>
+          <div class="commandArea">
+            <el-row class="loadingArea" v-loading="loading"
+                    element-loading-text="拼命加载中"
+                    element-loading-spinner="el-icon-loading"
+                    element-loading-background="rgba(0, 0, 0, 0.8)">
+              <el-input type="textarea" ref="input" class="commandBoard" rows="20" v-model="command" resize="none"
+                        v-on:keydown.delete.stop="banDelete($event)"></el-input>
+            </el-row>
+            <div class="buttonArea">
+              <el-button type="primary" v-on:click="sendCommand" icon="el-icon-thumb" :loading="loading">开始配置
+              </el-button>
+              <el-button type="primary" v-on:click="clearAll" icon="el-icon-remove-outline">清除所有
+              </el-button>
+            </div>
+          </div>
+        </el-main>
+      </el-container>
+    </div>
   </div>
 </template>
 
 <script>
+import ipcard from "@/components/ipCard";
 
 export default {
   name: "index",
+  components: {
+    ipcard
+  },
   data() {
     return {
       show: true,
@@ -36,7 +79,7 @@ export default {
     },
     async sendCommand() {
       this.loading = true;
-      await this.$axios.get('http://172.19.166.52:8080/gettelnet', {timeout: 100000}).then(res => {
+      await this.$axios.get('http://localhost:8080/test', {timeout: 1000 * 60 * 60}).then(res => {
         console.log(res.data);
         this.loading = false;
         this.$notify({
@@ -48,12 +91,15 @@ export default {
       }).catch(error => {
         this.$message({
           message: error,
-
           type: 'error',
           showClose: true
         });
       })
     },
+    // /**
+    //  * 根据commandList里命令自动配置
+    //  * @returns {Promise<void>}
+    //  */
     // async startConfigure() {
     //   let i = 0;
     //   for (i = 0; i < this.commandList.length; i++) {
@@ -76,19 +122,45 @@ export default {
 </script>
 
 <style scoped>
-.main {
+.container {
+  align-items: center;
+  justify-content: center;
+}
+
+.asideArea {
+  margin-left: 50px;
+}
+
+.configurationArea {
+  margin: 0 auto;
+}
+
+.box-card {
+  width: 565px;
+  height: 200px;
+}
+
+.loadingArea {
+  width: 600px;
+  margin-top: 20px;
+}
+
+.commandArea {
   width: 600px;
   margin: 0 auto;
-  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 
 .commandBoard {
-  margin-top: 30px;
   box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04)
 }
 
-
-.el-button {
+.buttonArea {
+  width: 400px;
   margin-top: 20px;
+  display: flex;
+  justify-content: space-around;
 }
 </style>
---
+++ src/components/ipCard.vue
@@ -0,0 +1,53 @@
+<template>
+  <el-card class="box-card">
+    <div slot="header">
+      <span>{{ name }}</span>
+      <el-button style="float: right; padding: 3px 0" type="text">确认修改</el-button>
+      <ip-card></ip-card>
+    </div>
+    <div class="item">
+      <div><span>ip:</span></div>
+      <div class="ipInput">
+        <el-input class="ip"></el-input>
+        <span>.</span>
+        <el-input class="ip"></el-input>
+        <span> . </span>
+        <el-input class="ip"></el-input>
+        <span> . </span>
+        <el-input class="ip"></el-input>
+      </div>
+    </div>
+  </el-card>
+</template>
+
+<script>
+export default {
+  name: "ipcard",
+  props:['name']
+
+}
+</script>
+
+<style scoped>
+.item {
+  display: flex;
+  height: 90px;
+  width: 530px;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+}
+
+.ipInput {
+  width: 430px;
+  margin-left: 20px;
+  display: flex;
+  flex-direction: row;
+  align-items: flex-end;
+  justify-content: space-between;
+}
+
+.ip {
+  width: 100px;
+}
+</style>
