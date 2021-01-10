903aabd042cf2c4a0465677fa1d04ed91c37a298
Lazy-Chowhound <1103036472@qq.com>
2021-01-10 15:32:35 +0800

    对ip进行格式检查，优化input显示

--- src/components/ipCard.vue
+++ src/components/ipCard.vue
@@ -2,19 +2,18 @@
   <el-card class="box-card">
     <div slot="header">
       <span>{{ name }}</span>
-      <el-button style="float: right; padding: 3px 0" type="text">确认修改</el-button>
-      <ip-card></ip-card>
+      <el-button style="float: right; padding: 3px 0" type="text" v-on:click="modify">确认修改</el-button>
     </div>
     <div class="item">
       <div><span>ip:</span></div>
       <div class="ipInput">
-        <el-input class="ip"></el-input>
+        <el-input class="ip" v-model="part1"></el-input>
         <span>.</span>
-        <el-input class="ip"></el-input>
-        <span> . </span>
-        <el-input class="ip"></el-input>
-        <span> . </span>
-        <el-input class="ip"></el-input>
+        <el-input class="ip" v-model="part2"></el-input>
+        <span>.</span>
+        <el-input class="ip" v-model="part3"></el-input>
+        <span>.</span>
+        <el-input class="ip" v-model="part4"></el-input>
       </div>
     </div>
   </el-card>
@@ -23,8 +22,55 @@
 <script>
 export default {
   name: "ipcard",
-  props:['name']
+  props: ['name'],
+  data() {
+    return {
+      part1: '',
+      part2: '',
+      part3: '',
+      part4: '',
+    }
+  },
+  methods: {
+    checkIp(ipPart) {
+      const ip = Number(ipPart)
+      return !(ipPart === "" || isNaN(ip) || ip % 1 !== 0 || ip < 0 || ip > 255);
+    },
+    modify() {
+      if (this.checkIp(this.part1) && this.checkIp(this.part2) && this.checkIp(this.part3) && this.checkIp(this.part4)) {
+        const ip = this.completeIp;
+        this.$confirm("是否确认更改ip", "提示", {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          //请求后端
 
+          this.$message({
+            type: 'success',
+            message: '修改成功!' + this.name + '的ip为:' + ip
+          })
+        }).catch(() => {
+          this.$message({
+            type: 'info',
+            message: '已取消修改'
+          });
+        })
+      } else {
+        this.$notify.error({
+          title: '配置错误',
+          message: 'ip格式出现了问题',
+          position: 'top-left',
+          offset: 50
+        })
+      }
+    }
+  },
+  computed: {
+    completeIp() {
+      return this.part1 + "." + this.part2 + "." + this.part3 + "." + this.part4;
+    }
+  }
 }
 </script>
 
@@ -49,5 +95,9 @@ export default {
 
 .ip {
   width: 100px;
+}
+
+.ip /deep/ .el-input__inner{
+  text-align: center;
 }
 </style>
